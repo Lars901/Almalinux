0d0db5ef649610c4e99540a2883032279d88f344
Lars901 <larsove_hoines@outlook.com>
2023-03-05 18:04:54 +0100

    Update Install.sh
    Add BraveBrowser, build requirements,VSCodium

--- Install.sh
+++ Install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 sudo dnf -y install pciutils
 sudo dnf install -y wget
+sudo dnf install -y nano
+sudo dnf install gzip2 tar
+#sudo nano/etc/dnf/dnf.conf
 #Requirements for fonts
 #dnf install -y rpm-build
 sudo dnf install -y ttmkfdir
@@ -34,4 +37,17 @@ sudo dnf -y install edk2-ovmf
  cd ~/GitHub/
  sudo chmod +x ./setup.sh
  ./setup.sh
+ #BraveBrowser
+sudo dnf install -y dnf-plugins-core
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo dnf install brave-browser
+
+#VSCodium
+sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
+sudo dnf install -y codium
+
+ #Requirements virtualbox guest additions
+ dnf install tar bzip2 kernel-devel-$(uname -r) kernel-headers perl gcc make elfutils-libelf-devel 
 sudo reboot
